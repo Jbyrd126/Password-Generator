e10f1c25c8ed63ee1013744dfd526432945bafbe
Jbyrd126 <jbyrd126@gmail.com>
2023-09-28 15:10:55 -0500

    Brandon helping me to understand and shorten some of the code

--- script.js
+++ script.js
@@ -7,63 +7,88 @@ const lettersUppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
 const lettersLowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 
-// Assignment Code
+// Assignment Code//
 var generateBtn = document.querySelector("#generate");
 
-// Add event listner to generate button
+// Add event listner to generate button//
 generateBtn.addEventListener("click", writePassword);
 
 // Write password to @password input
 function writePassword() {
-  var allowedPrompts = getPrompts();  // whether user inputs allowed items in prompts
+
+  // whether user inputs allowed items in prompts//
+  var allowedPrompts = getPrompts();
+
+
   var passwordText = document.querySelector("#password");
 
   if (allowedPrompts) {
     var userPassword = generatePassword();
     passwordText.value = userPassword;
+
+    //clears the box
   } else {
     passwordText.value = "";
   }
 }
-
+//creates password//
 function generatePassword() {
   var password = "";
-  for (var i = 0; i < characterLength; i++) {  //for loop to keep adding more chars
-    var randomChar = Math.floor(Math.random() * userArray.length);
-    password = password + userArray[randomChar]; // local var to hold userPassword
+
+  //for loop to keep adding more chars//
+  for (let i = 0; i < characterLength; i++) {
+    let randomChar = Math.floor(Math.random() * userArray.length);
+
+    // local var to hold userPassword//
+    password += userArray[randomChar];
 
   }
   return password;
 }
 
 function getPrompts() {
-  userArray = []; // start with empty array
 
-  characterLength = parseInt(prompt("How long do you want your password? (8 - 128 characters")); //how many chars does user want
+  // start with empty array//
+  userArray = [];
 
-  if (characterLength) characterLength <= 8  characterLength >= 128) {  // if correct parameters are chosen these should be false
+  //how many chars does user want//
+  characterLength = prompt("How long do you want your password? (8 - 128 characters");
+
+  // if correct parameters are chosen these should be false//
+  if (isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
     alert("Character length must be from 8-128 characters. C'mon man!")
     return false;
   }
-  if (confirm("Do you want lowercase letters in your password?")) {    //ask if they want these chars
+  //ask if they want these chars//
+  if (confirm("Do you want lowercase letters in your password?")) {
     userArray = userArray.concat(lettersLowercase);
 
   }
-
-  if (confirm("Do you want uppercase letters in your password?")) {   //ask if they want these chars
+  //ask if they want these chars//
+  if (confirm("Do you want uppercase letters in your password?")) {
     userArray = userArray.concat(lettersUppercase);
 
   }
-
-  if (confirm("Do you want numbers in your password?")) {   //ask if they want these chars 
+  //ask if they want these chars//
+  if (confirm("Do you want numbers in your password?")) {
     userArray = userArray.concat(numbers);
 
   }
-  if (confirm("Do you want symbols symbols in your password?")) {    //ask if they want these chars
-    userArray = userArray + (symbols);
+  //ask if they want these chars//
+  if (confirm("Do you want symbols symbols in your password?")) {
+    userArray = userArray.concat(symbols);
 
   }
-  return true;
+  if (userArray.length != 0) {
+    return true;
+
+  } else {
+
+    alert("You must choose a character.")
+
+    return false;
+  }
+
 
 
 
